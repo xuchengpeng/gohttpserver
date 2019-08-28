9000b13e1161af93fcb6567764476688ed9e19f8
xuchengpeng <330476629@qq.com>
2019-08-28 10:15:18 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ docker save --output gohttpserver.tar codeskyblue/gohttpserver
 git init
 git config user.name "xuchengpeng"
 git config user.email "330476629@qq.com"
+git lfs track "*.tar"
 git add .
 git commit -m "Release `date +"%Y-%m-%d %H:%M:%S"`"
 git push --force --quiet "https://${TRAVIS_TOKEN}@${GH_REF}" master:docker
