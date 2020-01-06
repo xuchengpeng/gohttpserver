7d6811aa05b1e16e01152b9587d380d9ec4186f7
xuchengpeng <330476629@qq.com>
2020-01-06 21:31:21 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,14 +11,3 @@ git lfs track "*.tar"
 git add .
 git commit -m "Release `date +"%Y-%m-%d %H:%M:%S"`"
 git push --force --quiet "https://${TRAVIS_TOKEN}@${GH_REF}" master:docker
-
-cd ~
-git clone https://github.com/xuchengpeng/org-site.git
-wget https://github.com/loveminimal/site/raw/master/js/darkreader.js
-cp darkreader.js org-site/js
-cd org-site
-git config user.name "xuchengpeng"
-git config user.email "330476629@qq.com"
-git add .
-git commit -m "add darkreader.js"
-git push --force --quiet "https://${TRAVIS_TOKEN}@github.com/xuchengpeng/org-site.git" master:master
